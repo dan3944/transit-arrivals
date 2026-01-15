c0aea56291fbce8aba6ddb97ff24d84140cee5b3
Dan Cohen <dccohe@gmail.com>
2026-01-15 18:24:04 -0500

    fix orientation

--- display.py
+++ display.py
@@ -96,6 +96,5 @@ class Display:
               size=18,
               anchor='mm')
 
-        img.transpose(Image.ROTATE_180)
-        self.inky_display.set_image(img)
+        self.inky_display.set_image(img.transpose(Image.ROTATE_180))
         self.inky_display.show()
