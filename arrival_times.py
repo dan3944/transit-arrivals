a0c3c85258da0573132282030384f38e4037830f
Dan Cohen <dccohe@gmail.com>
2026-02-01 13:05:58 -0500

    DisplayThread

--- arrival_times.py
+++ arrival_times.py
@@ -14,11 +14,7 @@ from data import Arrival, Route
 from display import Display
 
 
-<<<<<<< HEAD
-async def fetch_feed(url: str, session) -> gtfs_realtime_pb2.FeedMessage:
-=======
 async def _fetch_feed(url: str, session) -> gtfs_realtime_pb2.FeedMessage:
->>>>>>> buttons
     try:
         async with session.get(url=url) as response:
             feed = gtfs_realtime_pb2.FeedMessage()
@@ -28,71 +24,69 @@ async def _fetch_feed(url: str, session) -> gtfs_realtime_pb2.FeedMessage:
         logging.error(f'Exception when fetching from {url}: {e}')
         return gtfs_realtime_pb2.FeedMessage()
 
-class Controller:
-    def __init__(self, stop_ids: List[str], refresh_rate: int) -> None:
-        if not (1 <= len(stop_ids) <= 4):
-            raise Exception(f'stop_ids must have length between 1 and 4 inclusive (got {len(stop_ids)})')
 
-        self._stop_ids = stop_ids
-        self._refresh_rate = refresh_rate
-        with open('gtfs_subway/routes.txt') as f:
-            self._routes_by_id = {
-                line['route_id']: Route(
-                    id=line['route_id'],
-                    color='#' + line.get('route_color', 'ffffff'),
-                    text_color='#' + line.get('route_text_color', '000000'),
-                )
-                for line in csv.DictReader(f)
-            }
-        with open('gtfs_subway/stops.txt') as f:
-            self._stop_id_to_name = {
-                line['stop_id']: line['stop_name']
-                for line in csv.DictReader(f)
-            }
-
-    def stop_name(self, stop_id: str) -> str:
-        return self._stop_id_to_name.get(stop_id, stop_id)
-
-    def run(self):
-        chip = gpiodevice.find_chip_by_platform()
-        offset_to_stop_id = {
-            chip.line_offset_from_id(button): stop_id
-            for button, stop_id in zip([5, 6, 16, 24], self._stop_ids)
+def main(stop_ids: List[str], refresh_rate: int):
+    with open('gtfs_subway/routes.txt') as f:
+        routes_by_id = {
+            line['route_id']: Route(
+                id=line['route_id'],
+                color='#' + line.get('route_color', 'ffffff'),
+                text_color='#' + line.get('route_text_color', '000000'),
+            )
+            for line in csv.DictReader(f)
         }
-        request = chip.request_lines(consumer='spectra6-buttons', config={
-            offset: gpiod.LineSettings(direction=Direction.INPUT, bias=Bias.PULL_UP, edge_detection=Edge.FALLING)
-            for offset in offset_to_stop_id
-        })
-        current_stop_id = self._stop_ids[0]
-        thread, cancel = self._show_station_in_thread(current_stop_id)
+    with open('gtfs_subway/stops.txt') as f:
+        stop_id_to_name = {
+            line['stop_id']: line['stop_name']
+            for line in csv.DictReader(f)
+        }
 
-        while True:
-            for event in request.read_edge_events():
-                new_stop_id = offset_to_stop_id.get(event.line_offset)
-                if new_stop_id == current_stop_id:
-                    continue
+    chip = gpiodevice.find_chip_by_platform()
+    offset_to_stop_id = {
+        chip.line_offset_from_id(button): stop_id
+        for button, stop_id in zip([5, 6, 16, 24], stop_ids)
+    }
+    request = chip.request_lines(consumer='spectra6-buttons', config={
+        offset: gpiod.LineSettings(direction=Direction.INPUT, bias=Bias.PULL_UP, edge_detection=Edge.FALLING)
+        for offset in offset_to_stop_id
+    })
+    thread = DisplayThread(stop_ids[0], stop_id_to_name.get(stop_id, stop_id))
+    thread.start()
 
-                if new_stop_id is None:
-                    logging.error(f'Offset "{event.line_offset}" not found in {offset_to_stop_id}')
-                    continue
+    while True:
+        for event in request.read_edge_events():
+            stop_id = offset_to_stop_id.get(event.line_offset)
+            if stop_id == thread.stop_id:
+                continue
 
-                logging.info(f'Showing stop {self.stop_name(new_stop_id)} (stop_id: {new_stop_id})')
-                current_stop_id = new_stop_id
-                cancel.set()
-                thread.join()
-                thread, cancel = self._show_station_in_thread(current_stop_id)
+            if stop_id is None:
+                logging.error(f'Offset "{event.line_offset}" not found in {offset_to_stop_id}')
+                continue
 
-    def _show_station_in_thread(self, stop_id: str) -> tuple[threading.Thread, threading.Event]:
-        # We need to run _show_station() in a separate thread because read_edge_events()
-        # is blocking, so it will never yield the asyncio loop to other tasks.
-        cancel_event = threading.Event()
-        thread = threading.Thread(target=asyncio.run, args=(self._show_station(stop_id, cancel_event),))
-        thread.start()
-        return thread, cancel_event
+            stop_name = stop_id_to_name.get(stop_id, stop_id)
+            logging.info(f'Showing stop {stop_name} (stop_id: {stop_id})')
 
-    async def _show_station(self, stop_id: str, cancel_event: threading.Event):
-        stop_name = self.stop_name(stop_id)
-        display = Display(stop_name)
+            thread.terminate()
+            thread = DisplayThread(stop_id, stop_name)
+            thread.start()
+
+
+class DisplayThread:
+    def __init__(self, stop_id: str, name: str):
+        self.stop_id = stop_id
+        self.name = name
+        self._cancel = threading.Event()
+        self._thread = threading.Thread(target=asyncio.run, args=(self._show_station(),))
+
+    def start(self):
+        self._thread.start()
+
+    def terminate(self):
+        self._cancel.set()
+        self._thread.join()
+
+    async def _show_station(self):
+        display = Display(self._name)
         urls = [
             'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs',
             'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace',
@@ -106,7 +100,7 @@ class Controller:
         ]
 
         while not cancel_event.is_set():
-            logging.info(f'[{stop_name}] Fetching data...')
+            logging.info(f'[{self.name}] Fetching data...')
             async with aiohttp.ClientSession() as session:
                 feeds = await asyncio.gather(*(_fetch_feed(url, session) for url in urls))
 
@@ -117,13 +111,13 @@ class Controller:
                 for stop in entity.trip_update.stop_time_update
                 if stop.arrival.time
             ]
-            logging.info(f'[{stop_name}] Updating image')
+            logging.info(f'[{self.name}] Updating image')
             if cancel_event.is_set(): break
-            display.refresh(a for a in arrivals if a.stop_id == stop_id)
-            logging.info(f'[{stop_name}] Finished updating image')
+            display.refresh(a for a in arrivals if a.stop_id == self.stop_id)
+            logging.info(f'[{self.name}] Finished updating image')
             cancel_event.wait(timeout=self._refresh_rate)
 
-        logging.info(f'[{stop_name}] Cancelled')
+        logging.info(f'[{self.name}] Cancelled')
 
 
 '''
@@ -142,4 +136,6 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--stop_ids', required=True, nargs='+', help='The stop IDs to display (from stops.txt)')
     parser.add_argument('-r', '--refresh_rate', type=int, default=30, help='The refresh rate in seconds')
     args = parser.parse_args()
+    if not (1 <= len(args.stop_ids) <= 4):
+        raise Exception(f'stop_ids must have length between 1 and 4 inclusive (got {len(stop_ids)})')
     Controller(args.stop_ids, args.refresh_rate).run()
