967d6b02a8369d4e949800bc02605451d834c331
Dan Cohen <dccohe@gmail.com>
2026-02-01 13:29:39 -0500

    cleanup

--- arrival_times.py
+++ arrival_times.py
@@ -10,36 +10,17 @@ from gpiod.line import Bias, Direction, Edge
 from google.transit import gtfs_realtime_pb2
 from typing import List
 
-from data import Arrival, Route
+from data import Arrival
 from display import Display
 
 
-async def _fetch_feed(url: str, session) -> gtfs_realtime_pb2.FeedMessage:
-    try:
-        async with session.get(url=url) as response:
-            feed = gtfs_realtime_pb2.FeedMessage()
-            feed.ParseFromString(await response.read())
-            return feed
-    except Exception as e:
-        logging.error(f'Exception when fetching from {url}: {e}')
-        return gtfs_realtime_pb2.FeedMessage()
-
-
 def main(stop_ids: List[str], refresh_rate: int):
-    with open('gtfs_subway/routes.txt') as f:
-        routes_by_id = {
-            line['route_id']: Route(
-                id=line['route_id'],
-                color='#' + line.get('route_color', 'ffffff'),
-                text_color='#' + line.get('route_text_color', '000000'),
-            )
-            for line in csv.DictReader(f)
-        }
+    if not (1 <= len(stop_ids) <= 4):
+        raise ValueError(f'len(stop_ids) must be between 1 and 4 inclusive (got {len(stop_ids)})')
+
     with open('gtfs_subway/stops.txt') as f:
-        stop_id_to_name = {
-            line['stop_id']: line['stop_name']
-            for line in csv.DictReader(f)
-        }
+        stop_id_to_name = {line['stop_id']: line['stop_name']
+                           for line in csv.DictReader(f)}
 
     chip = gpiodevice.find_chip_by_platform()
     offset_to_stop_id = {
@@ -50,7 +31,7 @@ def main(stop_ids: List[str], refresh_rate: int):
         offset: gpiod.LineSettings(direction=Direction.INPUT, bias=Bias.PULL_UP, edge_detection=Edge.FALLING)
         for offset in offset_to_stop_id
     })
-    thread = DisplayThread(stop_ids[0], stop_id_to_name.get(stop_id, stop_id))
+    thread = DisplayThread(stop_ids[0], stop_id_to_name, refresh_rate)
     thread.start()
 
     while True:
@@ -63,20 +44,21 @@ def main(stop_ids: List[str], refresh_rate: int):
                 logging.error(f'Offset "{event.line_offset}" not found in {offset_to_stop_id}')
                 continue
 
-            stop_name = stop_id_to_name.get(stop_id, stop_id)
-            logging.info(f'Showing stop {stop_name} (stop_id: {stop_id})')
-
+            logging.info(f'Detected button press for stop_id {stop_id}')
             thread.terminate()
-            thread = DisplayThread(stop_id, stop_name)
+            thread = DisplayThread(stop_id, stop_id_to_name, refresh_rate)
             thread.start()
 
 
 class DisplayThread:
-    def __init__(self, stop_id: str, name: str):
+    def __init__(self, stop_id: str, stop_id_to_name: dict[str, str], refresh_rate: int):
         self.stop_id = stop_id
-        self.name = name
-        self._cancel = threading.Event()
+        self.name = stop_id_to_name.get(stop_id, stop_id)
+        self.refresh_rate = refresh_rate
+        # We need to run _show_station() in a separate thread because read_edge_events()
+        # is blocking, so it will never yield the asyncio loop to other tasks.
         self._thread = threading.Thread(target=asyncio.run, args=(self._show_station(),))
+        self._cancel = threading.Event()
 
     def start(self):
         self._thread.start()
@@ -86,38 +68,51 @@ class DisplayThread:
         self._thread.join()
 
     async def _show_station(self):
-        display = Display(self._name)
-        urls = [
-            'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs',
-            'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace',
-            'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-bdfm',
-            'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-g',
-            'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-jz',
-            'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-l',
-            'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-nqrw',
-            # Staten Island Railroad:
-            # 'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-si'
-        ]
+        display = Display(self.name)
 
-        while not cancel_event.is_set():
+        while not self._cancel.is_set():
             logging.info(f'[{self.name}] Fetching data...')
-            async with aiohttp.ClientSession() as session:
-                feeds = await asyncio.gather(*(_fetch_feed(url, session) for url in urls))
+            feeds = asyncio.run(_fetch_feeds([
+                'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs',
+                'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace',
+                'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-bdfm',
+                'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-g',
+                'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-jz',
+                'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-l',
+                'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-nqrw',
+                # Staten Island Railroad:
+                # 'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-si'
+            ]))
 
             arrivals = [
-                Arrival(stop, route=self._routes_by_id[entity.trip_update.trip.route_id])
+                Arrival(stop)
                 for feed in feeds
                 for entity in feed.entity
                 for stop in entity.trip_update.stop_time_update
                 if stop.arrival.time
             ]
             logging.info(f'[{self.name}] Updating image')
-            if cancel_event.is_set(): break
+            if self._cancel.is_set(): break
             display.refresh(a for a in arrivals if a.stop_id == self.stop_id)
             logging.info(f'[{self.name}] Finished updating image')
-            cancel_event.wait(timeout=self._refresh_rate)
+            self._cancel.wait(timeout=self.refresh_rate)
 
         logging.info(f'[{self.name}] Cancelled')
+
+
+async def _fetch_feeds(urls: List[str]) -> List[gtfs_realtime_pb2.FeedMessage]:
+    async with aiohttp.ClientSession() as session:
+        async def fetch_feed(url: str) -> gtfs_realtime_pb2.FeedMessage:
+            try:
+                async with session.get(url=url) as response:
+                    feed = gtfs_realtime_pb2.FeedMessage()
+                    feed.ParseFromString(await response.read())
+                    return feed
+            except Exception as e:
+                logging.error(f'Exception when fetching from {url}: {e}')
+                return gtfs_realtime_pb2.FeedMessage()
+
+        return await asyncio.gather(*(fetch_feed(url) for url in urls))
 
 
 '''
@@ -136,6 +131,4 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--stop_ids', required=True, nargs='+', help='The stop IDs to display (from stops.txt)')
     parser.add_argument('-r', '--refresh_rate', type=int, default=30, help='The refresh rate in seconds')
     args = parser.parse_args()
-    if not (1 <= len(args.stop_ids) <= 4):
-        raise Exception(f'stop_ids must have length between 1 and 4 inclusive (got {len(stop_ids)})')
-    Controller(args.stop_ids, args.refresh_rate).run()
+    main(args.stop_ids, args.refresh_rate)
